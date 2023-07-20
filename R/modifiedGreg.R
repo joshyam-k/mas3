6e940984c0b8c88bf2b5ef63baccd99ca1baf8fe
joshyam-k <yamamojo@reed.edu>
2023-07-20 14:08:27 -0700

    working version

--- R/modifiedGreg.R
+++ R/modifiedGreg.R
@@ -20,18 +20,183 @@ modifiedGreg.numeric <- function(y,
                                  N = NULL,
                                  messages = T) {
 
+
   .args <- as.list(environment())
   do.call(validate_modifiedGreg, .args)
   funcCall <- match.call()
 
+  if (is.null(N)) {
+    if (datatype == "raw") {
+      N <- nrow(xpop)
+    } else {
+      N <- sum(xpop$N)
+    }
+  }
+
+  if (is.null(domain_col_name)) {
+    if (datatype == "raw") {
+      domain_col_name <- setdiff(names(xpop), names(xsample))
+    } else {
+      domain_col_name <- setdiff(names(xpop), c(names(xsample), "N"))
+    }
+    if (messages) {
+      message(paste0("domain_col_name is not directly specified. ", domain_col_name, " is being used."))
+    }
+  }
+
+
+  if (is.null(pi)) {
+    pi <- rep(length(y)/N, length(y))
+  }
+
+  weight <- as.vector(pi^(-1))
+  y <- as.vector(y)
+
+  if (is.null(estimation_domains)) {
+    estimation_domains <- pop_unique_domains
+  }
+
+  # creating a vector of common auxiliary variable names
+  common_pred_vars <- intersect(names(xsample), names(xpop))
+
+  # creating the design matrix for entire xsample
+  xsample_d <- model.matrix(~., data = data.frame(xsample[common_pred_vars]))
+  xsample <- cbind(data.frame(xsample_d[,-1, drop = FALSE]), domains)
+  names(xsample) <- c(colnames(xsample_d[,-1, drop = FALSE]), domain_col_name)
+  xsample_dt <- t(xsample_d)
+
+  # variable selection
+
+  if (modelselect == TRUE) {
+    if(model == "linear"){
+      fam <- "gaussian"
+    } else{
+      fam <- "binomial"
+    }
+
+    x <- xsample[ , -which(names(xsample) == domain_col_name)]
+    cv <- cv.glmnet(x = as.matrix(x), y = y, alpha = 1, weights = weight, nfolds = 10, family = fam, standardize = FALSE)
+
+    if (lambda == "lambda.min") {
+      lambda.opt <- cv$lambda.min
+    }
+
+    if (lambda == "lambda.1se") {
+      lambda.opt <- cv$lambda.1se
+    }
+
+    pred_mod <- glmnet(x = as.matrix(x), y = y, alpha = 1, family = fam, standardize = FALSE, weights = weight)
+    lasso_coef <- predict(pred_mod, type = "coefficients", s = lambda.opt)[1:dim(xsample_d)[2],]
+    coef_select <- names(lasso_coef[lasso_coef != 0])[-1]
+
+    if (length(coef_select) == 0) {
+      if (messages) {
+        message("No variables selected in the model selection stage.  Fitting a HT estimator.")
+      }
+
+      if (var_est == TRUE) {
+        HT <- horvitzThompson(y = y, pi = pi, N = N, pi2 = pi2, var_est = TRUE, var_method = var_method)
+
+        return(list(pop_total = HT$pop_total,
+                    pop_mean = HT$pop_total/N,
+                    pop_total_var = HT$pop_total_var,
+                    pop_mean_var = HT$pop_total_var/N^2,
+                    weights = as.vector(pi^(-1))))
+      } else {
+        HT <- horvitzThompson(y = y, pi = pi, N = N, pi2 = pi2, var_est = FALSE)
+
+        return(list(pop_total = HT$pop_total,
+                    pop_mean = HT$pop_total/N,
+                    weights = as.vector(pi^(-1))))
+      }
+    } else {
+      xsample <- xsample[ , c(coef_select, domain_col_name), drop = FALSE]
+      xsample_d <- model.matrix(~., data = xsample[ , coef_select, drop = FALSE])
+      xsample_dt <- t(xsample_d)
+      xsample <- cbind(data.frame(xsample_d[,-1, drop = FALSE]), domains)
+      names(xsample) <- c(colnames(xsample_d[,-1, drop = FALSE]), domain_col_name)
+    }
+  }
+
+
+  if (model == "linear") {
+    if (datatype == "raw"){
+      # only using columns that occur in xsample too
+      xpop_subset <- xpop[common_pred_vars]
+
+      # expand factors and interaction terms and then re-add the domain column
+      xpop <- cbind(data.frame(model.matrix(~. -1, data = data.frame(xpop_subset))), xpop[domain_col_name])
+      xpop$`N` <- 1
+
+      # sum auxiliary pop values by domain
+      bydomain_formula <- as.formula(paste0(". ~", domain_col_name))
+      xpop_d <- aggregate(bydomain_formula, xpop, FUN = sum)
+      # move N column to the front (where it would normally be)
+      xpop_d <- xpop_d[, c(ncol(xpop_d), 1:(ncol(xpop_d) - 1))]
+    }
+
+    if (datatype == "totals"){
+      xpop_d <- xpop[ ,c("N", common_pred_vars, domain_col_name)]
+    }
+
+    if (datatype == "means"){
+      xpop[common_pred_vars] <- lapply(xpop[common_pred_vars], function(x) x*(xpop$N))
+      xpop_d <- cbind(N = xpop$N, xpop[ ,!(names(xpop) %in% "N")])
+    }
+
+    # computing the pieces that remain the same across all domains
+    constant_component1 <- solve(xsample_dt %*% diag(weight) %*% xsample_d)
+    constant_component2 <- t(weight * xsample_d)
+    betas <- solve(xsample_dt %*% diag(weight) %*% xsample_d) %*% (xsample_dt) %*% diag(weight) %*% y
+
+    res <- lapply(estimation_domains, FUN = by_domain_linear)
+
+  } else if (model == "logistic") {
+
+    if(length(levels(as.factor(y))) != 2){
+      stop("Function can only handle categorical response with two categories.")
+    }
+
+    xpop_subset <- xpop[common_pred_vars]
+    xpop <- cbind(data.frame(model.matrix(~., data = data.frame(xpop_subset))), xpop[domain_col_name])
+
+    # need N by domain
+    bydomain_formula <- as.formula(paste0(names(xpop)[1], "~", domain_col_name))
+    xpop_sums <- aggregate(bydomain_formula, xpop, FUN = sum)
+
+    # preparing logistic model
+    xsample_preds <- xsample[ , !(names(xsample) %in% domain_col_name)]
+    dat <- data.frame(y, weight, xsample_preds)
+    colnames(dat) <- c("y", "weight", names(xsample_preds))
+    f <- paste(names(dat)[1], "~", paste(names(dat)[-c(1,2)], collapse = " + "))
+    s_design <- survey::svydesign(ids = ~1, weights = ~weight, data = dat)
+    mod <- survey::svyglm(f, design = s_design, family = quasibinomial())
+
+    res <- lapply(estimation_domains, FUN = by_domain_logistic)
+
+  }
+
+  pop_res <- do.call(
+    rbind, lapply(
+      res, FUN = function(x) data.frame(
+        pop_total = x$domain_total,
+        pop_totalvar = x$domain_total_var
+      )
+    )
+  ) |>
+    colSums()
+
+  out <- list(
+    condition_res = res,
+    pop_res = pop_res
+  )
+
+  class(out) <- "modifiedGreg"
+  out
+
 }
 
 
-# modifiedGreg.data.frame <- function(y, ...) {
-#
-#   modifiedGreg.numeric(y[[1]], ...)
-#
-# }
 
 
 
--- R/utils.R
+++ R/utils.R
@@ -68,4 +68,155 @@ validate_modifiedGreg <- function(y,
     cli_abort(c("x" = "Incorrect number of columns in either xpop or xsample. When datatype != \"raw\" xpop should only contain columns with the same names as xsample as well as column with the domains and a column with the population sizes."))
   }
 
+  if (is.null(pi)) {
+    if (messages) {
+      message("Assuming simple random sampling")
+    }
+
+  }
+
+}
+
+
+varMase <- function(y, pi, pi2 = NULL, method = "LinHB", N = NULL){ #, fpc = fpc){
+
+  n <- length(y)
+
+  if(method=="LinHT" & is.null(pi2)){
+    cli_abort(c("x" = "For LinHT variance estimator, need to provide second order inclusion probabilities matrix."))
+  }
+
+  if(method == "LinHB"){
+    a <- n/(n-1)*(1-pi)
+    e <- as.vector(pi^(-1)*y) - c(sum(a)^(-1)*(pi^(-1)*a)%*%y)
+    varEst <- sum(a*e^2)
+  }
+
+  if(method == "LinHH"){
+    t <- pi^(-1)%*%y
+    varEst <- 1/(n*(n-1))*t(as.vector(n*y*pi^(-1)) - as.numeric(t))%*%(as.vector(n*y*pi^(-1)) - as.numeric(t))
+  }
+
+  if(method == "LinHTSRS"){
+    if(is.null(N)){
+      N <- sum(pi^(-1))
+    }
+    varEst <- (N-n)*(N/n)*var(y)
+  }
+
+  if(method== "LinHT"){
+    a <- (pi2 - pi%*%t(pi))*pi2^(-1)*(pi%*%t(pi))^(-1)*y%*%t(y)
+    varEst <- sum(a)
+  }
+
+  return(varEst)
+
+}
+
+
+by_domain_linear <- function(domain_id) {
+
+  domain_indic_vec <- as.integer(xsample[domain_col_name] == domain_id)
+
+  xpop_domain <- xpop_d[xpop_d[domain_col_name] == domain_id, , drop = FALSE]
+  xpop_d_domain <- unlist(xpop_domain[-which(names(xpop_domain) == domain_col_name)])
+  xsample_domain <- xsample[xsample[domain_col_name] == domain_id, , drop = FALSE]
+  xsample_d_domain <- model.matrix(~., data = data.frame(xsample_domain[common_pred_vars]))
+  xsample_dt_domain <- t(xsample_d_domain)
+  weights_domain <- weight[which(domain_indic_vec == 1)]
+
+  w <- as.matrix(
+    weight*domain_indic_vec + (
+      t(as.matrix(xpop_d_domain) - xsample_dt_domain %*% weights_domain) %*%
+        constant_component1
+    ) %*%
+      constant_component2
+  )
+
+  t <- w %*% y
+
+  domain_N <- unlist(xpop_domain["N"])
+
+  if (var_est == TRUE) {
+
+    if (var_method != "bootstrapSRS") {
+
+      y_hat <- xsample_d_domain %*% betas
+      y_domain <- y[which(domain_indic_vec == 1)]
+      e <- y_domain - y_hat
+      varEst <- varMase(y = e, pi = pi[which(domain_indic_vec == 1)], pi2 = pi2, method = var_method, N = domain_N)
+
+    } else if (var_method == "boostrapSRS"){
+
+      # need to implement
+
+    }
+
+    return(list(
+      domain = domain_id,
+      domain_total = as.numeric(t),
+      domain_mean = as.numeric(t)/as.numeric(domain_N),
+      domain_total_var = as.numeric(varEst),
+      domain_mean_var = as.numeric(varEst)/as.numeric(domain_N^2)
+    ))
+
+  } else {
+
+    return(list(
+      domain = domain_id,
+      domain_total = as.numeric(t),
+      domain_mean = as.numeric(t)/as.numeric(domain_N)
+    ))
+
+  }
+
+}
+
+by_domain_logistic <- function(domain_id) {
+
+  domain_indic_vec <- as.integer(xsample[domain_col_name] == domain_id)
+
+  xpop_domain <- xpop[xpop[domain_col_name] == domain_id, , drop = FALSE]
+  xsample_domain <- xsample[xsample[domain_col_name] == domain_id, , drop = FALSE]
+  domain_N <- xpop_sums[xpop_sums[domain_col_name] == domain_id, ,drop = FALSE][names(xpop)[1]]
+
+  y_hats_U <- as.matrix(predict(mod, newdata = xpop_domain[ , -1], type = "response", family = quasibinomial()))
+  y_hats_s <- as.matrix(predict(mod, newdata = xsample_domain, type = "response", family = quasibinomial()))
+
+  y_domain <- y[which(domain_indic_vec == 1)]
+  weights_domain <- weight[which(domain_indic_vec == 1)]
+
+  t <- t(y_domain - y_hats_s) %*% weights_domain + sum(y_hats_U)
+
+  if (var_est == TRUE) {
+
+    if (var_method != "bootstrapSRS") {
+
+      e <- y_domain - y_hats_s
+      varEst <- varMase(y = e, pi = pi[which(domain_indic_vec == 1)], pi2 = pi2, method = var_method, N = domain_N)
+
+    } else if (var_method == "bootstrapSRS") {
+
+      # need to implement
+
+    }
+
+    return(list(
+      domain = domain_id,
+      domain_total = as.numeric(t),
+      domain_mean = as.numeric(t)/as.numeric(domain_N),
+      domain_total_var = as.numeric(varEst),
+      domain_mean_var = as.numeric(varEst)/as.numeric(domain_N^2)
+    ))
+
+  } else {
+
+    return(list(
+      domain = domain_id,
+      domain_total = as.numeric(t),
+      domain_mean = as.numeric(t)/as.numeric(domain_N)
+    ))
+
+  }
+
 }
